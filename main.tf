c6ed70fbed947f0d409be039fe3b99152f13cd69
tauseef0007 <tauseef4962@gmail.com>
2026-04-28 10:36:00 +0530

    Change storage account name in Terraform config

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name = "myrg"
-    storage_account_name = "iphone"
+    storage_account_name = "indianboy"
     container_name = "mycontainer"
 key = "my.tfstate"
   }
